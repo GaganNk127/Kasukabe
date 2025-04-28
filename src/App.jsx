09b065bcd2c29ef302354eaf67c76f656ee21f9e
GaganNk127 <gagannaik127@gmail.com>
2025-04-28 15:07:39 +0530

    kkh

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,9 @@ import GradientText from './components/GradientText.jsx'
 import FlyingPosters from './components/FlyingPosters.jsx'
 import img3 from './assets/images/img3.jpg';
 import img4 from './assets/images/img4.jpg';
-import img5 from './assets/images/img1.jpg';
-import img6 from './assets/images/img1.jpg';
-import img7 from './assets/images/img1.jpg';
+import img5 from './assets/images/img5.jpg';
+import img6 from './assets/images/img6.jpg';
+import img7 from './assets/images/img7.jpg';
 import img8 from './assets/images/img8.jpg';
 import img9 from './assets/images/img9.jpg';
 import img10 from './assets/images/img10.jpg';
